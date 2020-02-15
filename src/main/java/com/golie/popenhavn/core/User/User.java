667862f3e63a0b3f15f46502593c3edf1b310acb
Nike <nike.bliznyuk@gmail.com>
2020-02-15 09:57:01 +0600

    minor refactor

--- src/main/java/com/golie/popenhavn/core/User/User.java
+++ src/main/java/com/golie/popenhavn/core/User/User.java
@@ -9,28 +9,23 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @EqualsAndHashCode(of = {"id"})
-
 public class User {
   private String id;
   private String email;
   private String userName;
-  private String password;
 
-  public User(String email, String userName, String password){
+
+  public User(String email, String userName) {
     this.id = UUID.randomUUID().toString();
     this.email = email;
     this.userName = userName;
-    this.password = password;
   }
 
-  public void update(String email, String userName, String password){
-    if(!"".equals(email))
+  public void update(String email, String userName) {
+    if (!"".equals(email))
       this.email = email;
 
-    if(!"".equals(userName))
+    if (!"".equals(userName))
       this.userName = userName;
-
-    if(!"".equals(password))
-      this.password = password;
   }
 }
